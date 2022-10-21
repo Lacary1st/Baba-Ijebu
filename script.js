11a455f20f1a57c5e0365e714d4cd08e59eae6a8
Oluwatomilola <oluwatomilola22@gmail.com>
2022-10-21 07:54:39 -1200

    done and submitting

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
 
 function play(){
     input = document.getElementById('games');           
-    let newNums =Array.from(input.value);
+    let newNums = Array.from(input.value);
     usersGames = newNums;
-
     const randomNums = [];
     for(let x =0; x < 6 ; x++) {
         randomNums[x] = Math.floor(Math.random() * 100)
- 
-document.getElementById('randomnums').innerHTML = randomNums
+        document.getElementById('randomnums').innerHTML = randomNums
     }
+
     const myGame = [];
-    let inputGames =0
+    let inputGames = 0
         for (let value of usersGames){
             let tonum = parseInt(value)
             if(randomNums.includes(tonum)){
                 myGame[inputGames] = value;
                 inputGames++
             }
-            document.getElementById('returnnums').innerHTML = myGame;
+            document.getElementById('games').innerHTML = myGame;
         }
-            if(myGame.length >= 3){
+            if(myGame.length >= 2){
                 document.getElementById('result').innerHTML   ='Congratulations! You win'
             }
             else{
